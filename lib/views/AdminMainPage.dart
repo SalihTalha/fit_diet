d9e036f2c014efca739554fbbf4f47fadceb64bf
RgSware <rahimgokceli78@gmail.com>
2021-12-18 23:40:18 +0300

    diet plannig sayfası düzenlendi

---
+++ lib/views/AdminMainPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AdminManinPage extends StatefulWidget {
+  @override
+  _AdminManinPageState createState() => _AdminManinPageState();
+}
+
+class _AdminManinPageState extends State<AdminManinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("ben main page kardeşşş"),
+      ),
+    );
+  }
+}
---
+++ lib/views/DietPlanningOperations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DietPlanningOperations extends StatefulWidget {
+  @override
+  _DietPlanningOperationsState createState() => _DietPlanningOperationsState();
+}
+
+class _DietPlanningOperationsState extends State<DietPlanningOperations> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("ben diet planlama operasyonları sayfası kardeşşş"),
+    );
+  }
+}
